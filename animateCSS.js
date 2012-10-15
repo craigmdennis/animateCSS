776bde10609faf8ed09f3c058344ce8e4b1eae6d
Craig Dennis <craigmdennis@gmail.com>
2012-10-15 23:50:29 +0100

    Allow mutiple animations without conflict thanks to @psyked

--- animateCSS.js
+++ animateCSS.js
@@ -9,7 +9,7 @@
             var $this = $(this);
 
             // Check if delay exists or if it's a callback
-            if (!delay || typeof delay == 'function') {
+            if (isNaN(delay) || typeof delay === 'function') {
 
                 // If it's a callback, move it to callback so we can call it later
                 callback = delay;
@@ -25,40 +25,49 @@
                 $this.addClass('animated ' + effect);
 
                 // Check if the elemenr has been hidden to start with
-                if ($this.css('visibility') == 'hidden') {
+                if ($this.css('visibility') === 'hidden') {
 
                     // If it has, show it (after the class has been added)
                     $this.css({
-                        'visibility': 'visible'
+                        'visibility' : 'visible'
                     });
 
                 }
 
                 // If the element is hidden
                 if ($this.is(':hidden')) {
-
                     // Show it
                     $this.show();
-
                 }
 
-                // Event triggered when the animation has finished
-                $this.bind('animationend webkitAnimationEnd oAnimationEnd', function () {
-                
+                function onEnd() {
+
+                    $this.unbind('animationend webkitAnimationEnd oAnimationEnd', onEnd);
+
                     // Remove the classes so they can be added again later
                     $this.removeClass('animated ' + effect);
 
                     // Add a callback event
-                    if (typeof callback == 'function') {
+                    if (typeof(callback) === 'function') {
 
                         // Execute the callback
                         callback.call(this);
 
                     }
 
-                });
+                }
 
-            // Specify the delay
+                // Event triggered when the animation has finished
+                $this.unbind('animationend webkitAnimationEnd oAnimationEnd', onEnd).bind('animationend webkitAnimationEnd oAnimationEnd', onEnd);
+
+                if (!$("html").hasClass("csstransforms")) {
+
+                    onEnd();
+
+                }
+
+                // Specify the delay
+                
             }, delay);
 
         });
