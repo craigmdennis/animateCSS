3b03bb9324e88713adc61d518049a80f016fa4ea
Craig Dennis <craigmdennis@gmail.com>
2012-11-26 15:58:10 +0000

    If there's no delay, the animation no longer runs inside the setTimeout

--- animateCSS.js
+++ animateCSS.js
@@ -8,67 +8,72 @@
             // Cache $(this) for speed
             var $this = $(this);
 
-            // Check if delay exists or if it's a callback
-            if (isNaN(delay) || typeof delay === 'function') {
-
-                // If it's a callback, move it to callback so we can call it later
-                callback = delay;
-
-                // Set the delay to 0 for the setTimeout
-                delay = 0;
-            }
-
-            // Start a counter so we can delay the animation if required
-            var animation = setTimeout(function () {
+            // Create a function we can call later
+            function run() {
 
                 // Add the animation effect with classes
                 $this.addClass('animated ' + effect);
 
                 // Check if the elemenr has been hidden to start with
-                if ($this.css('visibility') === 'hidden') {
+                if ($this.css('visibility') == 'hidden') {
 
                     // If it has, show it (after the class has been added)
                     $this.css({
-                        'visibility' : 'visible'
+                        'visibility': 'visible'
                     });
 
                 }
 
                 // If the element is hidden
                 if ($this.is(':hidden')) {
+
                     // Show it
                     $this.show();
-                }
-
-                function onEnd() {
-
-                    $this.unbind('animationend webkitAnimationEnd oAnimationEnd', onEnd);
-
-                    // Remove the classes so they can be added again later
-                    $this.removeClass('animated ' + effect);
-
-                    // Add a callback event
-                    if (typeof(callback) === 'function') {
-
-                        // Execute the callback
-                        callback.call(this);
-
-                    }
 
                 }
 
                 // Event triggered when the animation has finished
-                $this.unbind('animationend webkitAnimationEnd oAnimationEnd', onEnd).bind('animationend webkitAnimationEnd oAnimationEnd', onEnd);
+                $this.bind('animationend webkitAnimationEnd oAnimationEnd', function () {
+                
+                    // Remove the classes so they can be added again later
+                    $this.removeClass('animated ' + effect);
 
-                if (!$("html").hasClass("csstransforms")) {
+                    // Add a callback event
+                    if (typeof callback == 'function') {
 
-                    onEnd();
+                        // Execute the callback
+                        callback.call(this);
 
-                }
+                        // Unbind the event handlers
+                        $this.unbind('animationend webkitAnimationEnd oAnimationEnd');
+
+                    }
+
+                });
+
+            }
+
+            // Check if delay exists or if it's a callback
+            if (!delay || typeof delay == 'function') {
+
+                // If it's a callback, move it to callback so we can call it later
+                callback = delay;
+
+                // Run the animation (without delay)
+                run();
+
+            } else {
+
+                // Start a counter so we can delay the animation if required
+                var animation = setTimeout(function () {
+
+                    // Run the animation (with delay)
+                    run();
 
                 // Specify the delay
-                
-            }, delay);
+                }, delay);
+
+            }
 
         });
 
