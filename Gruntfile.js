3b4997ad270d83a3e4befaa6365cacc3474fc94e
Craig Dennis <craigmdennis@gmail.com>
2014-05-21 17:34:41 +0100

    chore: add changelog to grunt-bump commits

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,7 +111,7 @@ module.exports = function (grunt) {
 
     bump: {
       options: {
-        files: ['package.json', 'bower.json', '<%= pkg.name %>.jquery.json'],
+        files: ['package.json', 'bower.json', '<%= pkg.name %>.jquery.json', 'CHANGELOG.md'],
         push: true,
         pushTo: 'origin',
         createTag: true,
